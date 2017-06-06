20c5e2d39e0766b50be2b2d49abfdcdfc7e328e3
Mark Ronquillo <markronquillo23@gmail.com>
2017-06-06 23:05:00 +0800

    Add AWS related configuration

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,18 +1,16 @@
-require('dotenv').config();
-
 export default {
-	MAX_ATTACHMENT_SIZE: 5000000,
-	apiGateway: {
-		URL: process.env.API_GATEWAY_URL,
-	},
+  MAX_ATTACHMENT_SIZE: 5000000,
+  apiGateway: {
+    URL: 'https://jxczb5zwna.execute-api.us-east-1.amazonaws.com/prod',
+  },
   cognito: {
-    USER_POOL_ID : process.env.USER_POOL_ID,
-    REGION: process.env.REGION,
-    APP_CLIENT_ID : process.env.APP_CLIENT_ID,
-    IDENTITY_POOL_ID: process.env.IDENTITY_POOL_ID,
+    USER_POOL_ID : 'us-east-1_72X5RNuLb',
+    REGION: 'us-east-1',
+    APP_CLIENT_ID : '56mhb72fet3aeab3pfuce4jg49',
+    IDENTITY_POOL_ID: 'us-east-1:1d3b4afc-bfeb-4a3b-92bb-c006411b5ed2',
   },
   s3: {
-  	BUCKET: process.env.BUCKET
+    BUCKET: 'markronquillo-notes-app'
   }
 };
 
